8c30a9d7e8df2babb9f46fcdcf05b92e1e834222
ren-angel <d_medrado@yahoo.com>
2026-06-01 17:07:25 -0300

    Testando cidade

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -13,10 +13,10 @@ const isCityValid = async (value) => {
 
   if (fetchCity.type === "city_error") {
 
-    return fetchCity;
+    return fetchCity.json();
   } else {
 
-    return fetchCity;
+    return fetchCity.json();
   }
 }
 
